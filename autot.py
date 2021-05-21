e55dc7ccad3b489818a145ec6bcdb803792dc24d
tbvjvjvl <84087494+tbvjvjvl@users.noreply.github.com>
2021-05-22 04:07:07 +0900

    Update autot.py

--- autot.py
+++ autot.py
@@ -7,19 +7,19 @@ secret = "tewQC1itbu3RgnVKSFxI3IyAzPPszDnPMlS4CMx0"
 
 def get_target_price(ticker, k):
     """변동성 돌파 전략으로 매수 목표가 조회"""
-    df = pyupbit.get_ohlcv(ticker, interval="minute15", count=2)
+    df = pyupbit.get_ohlcv(ticker, interval="day", count=2)
     target_price = df.iloc[0]['close'] + (df.iloc[0]['high'] - df.iloc[0]['low']) * k
     return target_price
 
 def get_start_time(ticker):
     """시작 시간 조회"""
-    df = pyupbit.get_ohlcv(ticker, interval="minute15", count=1)
+    df = pyupbit.get_ohlcv(ticker, interval="day", count=1)
     start_time = df.index[0]
     return start_time
 
 def get_ma9(ticker):
     """15일 이동 평균선 조회"""
-    df = pyupbit.get_ohlcv(ticker, interval="minute15", count=9)
+    df = pyupbit.get_ohlcv(ticker, interval="day", count=9)
     ma9 = df['close'].rolling(9).mean().iloc[-1]
     return ma9
 
@@ -46,10 +46,10 @@ while True:
     try:
         now = datetime.datetime.now()
         start_time = get_start_time("KRW-ETC")
-        end_time = start_time + datetime.timedelta(minutes=15)
+        end_time = start_time + datetime.timedelta(days=1)
 
         if start_time < now < end_time - datetime.timedelta(seconds=10):
-            target_price = get_target_price("KRW-ETC", 0.9)
+            target_price = get_target_price("KRW-ETC", 0.5)
             ma9 = get_ma9("KRW-ETC")
             current_price = get_current_price("KRW-ETC")
             if target_price < current_price and ma9 < current_price:
@@ -57,9 +57,9 @@ while True:
                 if krw > 5000:
                     upbit.buy_market_order("KRW-ETC", krw*0.9995)
         else:
-            eth = get_balance("ETC")
-            if eth > 0.0009:
-                upbit.sell_market_order("KRW-ETC", eth*0.9995)
+            etc = get_balance("ETC")
+            if etc > 0.06:
+                upbit.sell_market_order("KRW-ETC", etc*0.9995)
         time.sleep(1)
     except Exception as e:
         print(e)
